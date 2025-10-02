c05ef862c098b46750e28b392a2fe82714a8d48a
Esthiber <esthiber1@gmail.com>
2025-10-01 21:07:36 -0400

    Se creo HiltApp HuacalesApp

---
+++ app/src/main/java/edu/ucne/esthibervalentin_ap2_p1/RegistroHuacalesApp.kt
@@ -0,0 +1,8 @@
+package edu.ucne.esthibervalentin_ap2_p1
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RegistroHuacalesApp: Application() {
+}
